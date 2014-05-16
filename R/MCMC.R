4744991e8b7850f73cc07204ba345af80b60f428
RobHayward <robohay@gmail.com>
2014-05-16 06:48:53 +0100

    Updated the sample MCM.

--- R/MCMC.R
+++ R/MCMC.R
@@ -1,18 +1,21 @@
 # Create a simple MCMC sampler
-x <- seq(1, 10, by = 1)
+x <- seq(1, 100, by = 1)
 e <- rnorm(length(x))
 y <- 0.5 + 5 * x + e
 plot(y ~ x)
 y
 # prior 
+a <- rep(NA, times = length(x))
+b <- rep(NA, times = length(x))
 a[1] = 1
 b[1] = 1
-for(i in 1:10){
-b[i] <- (y[i] - a[i] - rnorm(1))/x[i]
-b
-a[i] <-  y - b[i]*x[i] - rnorm(1)
+for(i in 2:100){
+b[i] <- (y[i] - a[i-1] - rnorm(1))/x[i]
+a[i] <-  y[i] - b[i]*x[i] - rnorm(1)
 }
 a
 b
-plot(y ~ x)
+plot(y ~ x, type = 'l')
 abline(a = mean(a), b = mean(b))
+lines(fitted(lm(y ~x)), col = 'red')
+  
